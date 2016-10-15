2c7b1f48b5658524d31b9864b0d5afc3022821cb
William Schroeder <wschroeder@gmail.com>
2016-10-14 23:51:16 -0500

    Demonstrate basic spawning and JS interop.

--- src/clj_screeps/core.cljs
+++ src/clj_screeps/core.cljs
@@ -2,6 +2,33 @@
 
 (enable-console-print!)
 
-(defn ^:export on-tick []
-  (println "Hello world!"))
+(defn- inspect
+  ; Log to the console the passed-in value, and return the value.  This
+  ; basically allows one to insert this debug printer anywhere by simply
+  ; wrapping forms with its call.
+
+  ([value]
+   (js/console.log value)
+   value)
+  ([value message]
+   (js/console.log message value)
+   value))
+
+(defn ^:export on-tick
+  ; Screeps calls this script periodically.  Our goal for this version is to
+  ; spawn a creep.  We convert this JavaScript:
+  ;
+  ;     Game.spawns['Spawn1'].createCreep( [WORK, CARRY, MOVE], 'Harvester1' );
+  ;
+  ; to a ClojureScript equivalent.  A dirt-simple approach would just be:
+  ;
+  ;     (js/Game.spawns.Spawn1.createCreep #js [js/WORK js/CARRY js/MOVE] "Harvester1")
+  ;
+  ; But what if, instead of "Spawn1", our spawn was called "My Spawn"?  You
+  ; would need to use the noisier `aget` approach, which is demonstrated here.
+
+  []
+  (-> js/Game
+      (aget "spawns" "Spawn1")
+      (.createCreep #js [js/WORK js/CARRY js/MOVE] "Harvester1")))
 
