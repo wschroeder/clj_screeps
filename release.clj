9b2b5f432583c3b818caa41532fc86e550622504
William Schroeder <wschroeder@gmail.com>
2016-10-14 23:43:40 -0500

    Do not mangle explicit JS names!

--- release.clj
+++ release.clj
@@ -2,7 +2,13 @@
 
 (cljs.build.api/build "src"
   {:output-to "out/clj.js"
-   :optimizations :advanced})
+   :optimizations :simple})
+
+; NOTE: Do not be tempted to use :optimizations :advanced.  They are anything
+; but.  They mangle the JavaScript names, breaking advertised contracts with
+; what you should expect out of ClojureScript's JavaScript interop.  Hopefully
+; this is resolved in a future version of ClojureScript.  We choose :simple in
+; order to generate a single script file as our entrypoint into Screeps.
 
 (System/exit 0)
 
